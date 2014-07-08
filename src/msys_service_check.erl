3cf3095b7593b47ceb05a63f8e362d3204d60e07
Sebastian Green-Husted <ricecake@tfm.nu>
2014-07-08 18:54:26 -0400

    Update msys_service_check.erl

--- src/msys_service_check.erl
+++ src/msys_service_check.erl
@@ -1,1 +1,14 @@
 -module(msys_service_check).
+
+-fields([
+  id,
+  host_id,
+  service_id,
+  status,
+  status_start
+]).
+
+-join([
+  {belongs_to, msys_host, host_id},
+  {belongs_to, msys_service, service_id}
+]).
