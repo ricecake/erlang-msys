d68ceecbc7537dcd7c71e17ddf727bde56a605cb
Sebastian Green-Husted <ricecake@tfm.nu>
2014-07-14 12:36:14 -0400

    Update msys_check.erl

--- src/msys_check.erl
+++ src/msys_check.erl
@@ -1,1 +1,13 @@
 -module(msys_check).
+
+
+%% ------------------------------------------------------------------
+%% Behaviour Definition
+%% ------------------------------------------------------------------
+
+behaviour_info(callbacks) -> 
+        [
+    {init, 1},
+    {check, 3}
+];
+behaviour_info(_Other) -> undefined.
