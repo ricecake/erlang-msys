99b435675acde5ca0e0676b308853d58015922f2
Sebastian Green-Husted <ricecake@tfm.nu>
2014-07-17 19:57:58 -0400

    Create msys_check_http.erl

---
+++ src/msys_check_http.erl
@@ -0,0 +1,9 @@
+-module(msys_check_http).
+-behavior(msys_check).
+
+-export([init/1, check/2]).
+
+init(_Args) -> ok.
+
+check(HostInfo, CheckInfo) ->
+  {ok,{{_, Code, Status}, _, _}} = httpc:request(head, {"http://google.com", []}, [], []).
