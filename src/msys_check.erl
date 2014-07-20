50e5ec838a6977647c0c750e74c7542f48ddaeea
Sebastian Green-Husted <ricecake@tfm.nu>
2014-07-19 23:35:15 -0400

    Update msys_check.erl

--- src/msys_check.erl
+++ src/msys_check.erl
@@ -6,7 +6,7 @@
 
 behaviour_info(callbacks) -> 
         [
-                {init, 1},
+                {init, 1}, %called at application start to prep report action
                 {check, 3}
         ];
 behaviour_info(_Other) -> undefined.
