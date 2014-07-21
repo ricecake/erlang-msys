9f6723738808bbe298126e01b81e8b91a4b8bad2
Sebastian Green-Husted <ricecake@tfm.nu>
2014-07-20 22:14:51 -0400

    Update msys_check_action.erl

--- src/msys_check_action.erl
+++ src/msys_check_action.erl
@@ -6,7 +6,7 @@
 
 behaviour_info(callbacks) -> 
         [
-                {init, 1},
+                {init, 1}, % aplicatiln startup system init
                 {report, 3}
         ];
 behaviour_info(_Other) -> undefined.
