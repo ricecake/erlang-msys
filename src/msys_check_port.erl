ae86a510a3dc626b7398b22485809ea06cf63412
Sebastian Green-Husted <ricecake@tfm.nu>
2014-09-15 20:11:02 -0400

    start of simple port check

---
+++ src/msys_check_port.erl
@@ -0,0 +1,16 @@
+-module(msys_check_port).
+-behavior(msys_check).
+
+-export([init/1, check/2]).
+
+init(_Args) -> ok.
+
+check(HostInfo, CheckInfo) -> {ok, {up, []}}.
+
+
+ping(Host, Port) ->
+ case (gen_tcp:connect(Host, Port, [binary, {packet, 0}], 1000)) of
+   {ok, Socket} -> {gen_tcp:close(Socket), {Port, open}};
+   {error, _Reason} -> {ok, {Port, closed}};
+   Error -> {error, Error}
+ end.
