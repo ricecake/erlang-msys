ddad74369d05a08b4191077fa25895bccada6cb5
Sebastian Green-Husted <ricecake@tfm.nu>
2014-10-03 18:35:55 -0400

    start of getting app logic setup

--- src/msys_app.erl
+++ src/msys_app.erl
@@ -10,7 +10,30 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    msys_sup:start_link().
+	% Ensure That We start the supervisor before initing the tools,
+	% So that it will be available to the various pieces.
+	{ok, Pid} = msys_sup:start_link(),
+	[ ok = msys_check:init(Check) || Check <- checks() ],
+	[ ok = msys_action:init(Action) || Action <- actions() ],
+	[ ok = System:init() || System <- systems() ],
+	{ok, Pid}.
 
 stop(_State) ->
     ok.
+
+
+systems() -> [
+	msys_host,
+	msys_node,
+	msys_service,
+	msys_service_check,
+	msys_check_queue
+].
+
+actions() -> [
+	msys_action_ws
+].
+
+checks() -> [
+	msys_check_port
+].
