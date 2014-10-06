260605fca342d34909bf3c8baad7807ad84b7992
Sebastian Green-Husted <ricecake@tfm.nu>
2014-10-05 23:50:28 -0400

    correct some accident code back to where it should have been

--- src/msys_action_ws.erl
+++ src/msys_action_ws.erl
@@ -1,33 +1,15 @@
 -module(msys_action_ws).
 
 %-export([init/1, report/3]).
--module(fingwb_app).
 
--behaviour(application).
-
-%% Application callbacks
--export([start/2, stop/1]).
-
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
-
-start(_StartType, _StartArgs) ->
-    fingwb_whiteboard:init([]),
-    {ok, Pid} = fingwb_sup:start_link(),
+init(_Opts) ->
     Dispatch = cowboy_router:compile([
 	    {'_', [
-			{"/", fingwb_index, []},
-			{"/new", fingwb_create, []},
-			{"/ws/:whiteboard_id", fingwb_msg_handler, []},
-			{"/:whiteboard_id", fingwb_canvas, []},
-			{"/js/[...]", cowboy_static, {priv_dir, fingwb, "js/"}},
-			{"/css/[...]", cowboy_static, {priv_dir, fingwb, "css/"}}
+			{"/js/[...]", cowboy_static, {priv_dir, msys, "js/"}},
+			{"/css/[...]", cowboy_static, {priv_dir, msys, "css/"}}
 		]}
 	]),
 	{ok, _} = cowboy:start_http(http, 25, [{ip, {127,0,0,1}}, {port, 8080}],
         				[{env, [{dispatch, Dispatch}]}]),
 	{ok, Pid}.
 
-stop(_State) ->
-    ok.
