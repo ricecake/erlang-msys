ec3939c35698c38f9523e90ac33da40f30bb6fe0
Sebastian Green-Husted <ricecake@tfm.nu>
2014-10-11 23:39:07 -0400

    some helper functions for queue module

--- src/msys_check_queue.erl
+++ src/msys_check_queue.erl
@@ -1,11 +1,15 @@
 -module(msys_check_queue).
 -behavior(msys_system).
 
--export([init/1]).
+-export([setup/1]).
 
 -record(chkq, {
 	runtime,
 	check_id
 }).
 
+checkQueue() -> {ok, []}.
 
+scheduleNextRun() -> ok.
+
+scheduleCheck(Interval, CheckID) -> ok.
