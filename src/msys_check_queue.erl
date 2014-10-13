86bb0bd0205846b43d2ba700b354e0fc0bd62503
Sebastian Green-Husted <ricecake@tfm.nu>
2014-10-12 22:45:09 -0400

    gen server callbacks for check queue

--- src/msys_check_queue.erl
+++ src/msys_check_queue.erl
@@ -1,6 +1,8 @@
 -module(msys_check_queue).
 -behavior(msys_system).
+-behaviour(gen_server).
 
+-define(SERVER, ?MODULE).
 -export([setup/1]).
 
 -record(chkq, {
@@ -8,8 +10,56 @@
 	check_id
 }).
 
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+
+-export([start_link/0]).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Exports
+%% ------------------------------------------------------------------
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+
+init(Args) ->
+    {ok, Args}.
+
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% ------------------------------------------------------------------
+%% Internal Function Definitions
+%% ------------------------------------------------------------------
+
 checkQueue() -> {ok, []}.
 
 scheduleNextRun() -> ok.
 
 scheduleCheck(Interval, CheckID) -> ok.
+
+
