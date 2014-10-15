04750a3666d5cc0aecc780a5b2ae42845a012099
Sebastian Green-Husted <ricecake@tfm.nu>
2014-10-15 19:38:10 -0400

    use less precise timestamps

--- src/msys_check_queue.erl
+++ src/msys_check_queue.erl
@@ -65,5 +65,5 @@ popQueue() -> {ok, []}.
 
 scheduleNextRun() -> ok.
 
-timestamp() -> {Mega, Secs, Micro} = erlang:now(),  Mega*1000*1000*1000*1000 +Secs * 1000 * 1000 + Micro.
+timestamp() -> {Mega, Secs, _Micro} = erlang:now(),  Mega*1000*1000 +Secs.
 
